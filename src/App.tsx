0b4942653544f182694b79a2afc5697d2f45c850
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-31 21:24:17 +0530

    Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import useFetch from "./hooks/useFetch";
 import "./styles.css";
 export default function App() {
+  const { data: joke, loading, error } = useFetch(
+    "https://v2.jokeapi.dev/joke/Any"
+  );
+  if (loading) return <h2>Loading...</h2>;
+  if (error) console.log(error);
   return (
     <div className="App">
       <h1>useFetch Custom Hook</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>
+        {joke?.setup} : {joke?.delivery}
+      </h2>
     </div>
   );
 }
